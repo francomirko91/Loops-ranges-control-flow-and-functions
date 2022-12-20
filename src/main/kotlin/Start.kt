7658d02eb220b33220fcaaa7e71ae81cdc0a07f5
Mirko Franco <francomirko91@gmail.com>
2022-12-20 13:54:58 +0100

    ----------------

--- src/main/kotlin/Start.kt
+++ src/main/kotlin/Start.kt
@@ -2,8 +2,8 @@ fun main() {
 
     for (i in 1..100 step 2) {
         when {
-            i < 50 -> println("before 50 $i")
-            else -> println("after 50 $i")
+            i < 50 -> println("numbers below 50: $i")
+            else -> println("number above 50: $i")
         }
     }
 }
